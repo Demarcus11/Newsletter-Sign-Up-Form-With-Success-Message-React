b571e9a177b2e064205a36ea5bddd82b61ef77c2
Demarcus Hollman <demarcushollman1@gmail.com>
2023-05-29 21:23:06 -0400

    fix: image path

--- src/components/ThankYouModal.jsx
+++ src/components/ThankYouModal.jsx
@@ -1,7 +1,9 @@
+import { ListIcon } from "../../public/assets/images/icon-list.svg";
+
 export function ThankYouModal({ email, setShowThankYouModal }) {
   return (
     <div className="thank-you | container">
-      <img src="public\assets\images\icon-list.svg" alt="" />
+      <img src={ListIcon} alt="" />
       <div className="thank-you__details | content-flow">
         <h1 className="thank-you__title">Thanks for subscribing!</h1>
         <p>
